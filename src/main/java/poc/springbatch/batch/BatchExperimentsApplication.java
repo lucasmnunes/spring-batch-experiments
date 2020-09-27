55e2508abb4947005c044abf12c5d4bb5aad91fc
Lucas Nunes <lucasnunes000@gmail.com>
2020-09-27 10:50:52 -0300

    Configuração inicial: Spring Boot, Spring Batch e H2 Database

---
+++ src/main/java/poc/springbatch/batch/BatchExperimentsApplication.java
@@ -0,0 +1,13 @@
+package poc.springbatch.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchExperimentsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BatchExperimentsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/poc/springbatch/batch/BatchExperimentsApplicationTests.java
@@ -0,0 +1,13 @@
+package poc.springbatch.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchExperimentsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
